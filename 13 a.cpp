b4ad2fa35eb20fd39100e354816f981006fbf37b
medhakasture <59600906+medhakasture@users.noreply.github.com>
2021-07-12 22:48:22 +0530

    Update 13 a.cpp

--- 13 a.cpp
+++ 13 a.cpp
@@ -3,10 +3,10 @@ using namespace std;
 int main() {
     int rows;
     cin >> rows;
-    for(int i = 0; i < rows; i++) {
+    for(int i = 1; i <= rows; i++) {
         for(int space = 1; space <= rows-i; space++)
             cout <<"  ";
-        for(int j = 0; j <= i; j++)
+        for(int j = 1; j <= i; j++)
             cout << i << "   ";
         cout << endl;
     }
